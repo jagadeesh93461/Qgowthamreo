5f0a6641e67cc1aa46aff6302537790d8d66d599
jagadeesh93461 <konetijagadeesh7@gmail.com.com>
2025-07-08 22:02:12 +0530

    application.java

--- application.java
+++ application.java
@@ -6,3 +6,16 @@ public class Addition {
         System.out.println("Sum: " + sum);
     }
 }
+# feature 201 code by mr vivek
+public class IfElseCheck {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number >= 0) {
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Negative number");
+        }
+    }
+}
+# end the code
